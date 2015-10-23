f607bb6f19239edff325421c99ac2a2048d5160e
Tyler <tylerscottcrosse@gmail.com>
2015-10-23 10:48:47 -0400

    completed game without deciding who whins

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ var clickCount = 0;
 
 $(document).ready(function() {
 
-  // var turn = function(){
-  //   if (clickCount % 2 === 0) {
-  //     console.log("x");
-  //   }
-  // };
-  // on click add or remove class .active to td's
+  // "__'s turn"
 
   $("td").on("click", function() {
-    // if ($(this).hasClass("active")) {
-    //   $(this).removeClass("active");
-    // } else {
     $(this).addClass("active");
     clickCount++;
     console.log(clickCount);
     if (clickCount % 2 === 0) {
       console.log("o");
+      $(this).addClass("o");
+      $(this).html("<h1>O</h1>");
+      $("#whosturn").html("X's");
     }
     if (clickCount % 2 !== 0) {
       console.log("x");
+      $(this).addClass("x");
+      $(this).html("<h1>X</h1>");
+      $("#whosturn").html("O's");
     }
   });
 
   //when you press reset remove class active from all td's
   $(".reset").on("click", function() {
-    $("td").removeClass("active");
+    $("td").removeClass();
+    $("td").html("");
+    clickCount = 0;
   });
 
 });

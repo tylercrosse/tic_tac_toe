426bafa7affcc59786ac545e93b93ab50bd6d130
Tyler <tylerscottcrosse@gmail.com>
2015-10-23 11:43:11 -0400

    fixed click square bug

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 var clickCount = 0;
+var getVals = function() {
+  var row1 = [];
+  row1.push($("#e11").attr("class"));
+  row1.push($("#e13").attr("class"));
+  row1.push($("#e12").attr("class"));
+  console.log(row1);
+};
 
 $(document).ready(function() {
 
   // "__'s turn"
 
   $("td").on("click", function() {
-    $(this).addClass("active");
-    clickCount++;
-    console.log(clickCount);
-    if (clickCount % 2 === 0) {
-      console.log("o");
-      $(this).addClass("o");
-      $(this).html("<h1>O</h1>");
-      $("#whosturn").html("X's");
-    }
-    if (clickCount % 2 !== 0) {
-      console.log("x");
-      $(this).addClass("x");
-      $(this).html("<h1>X</h1>");
-      $("#whosturn").html("O's");
+    if (!$(this).hasClass("x") && !$(this).hasClass("o")) {
+      clickCount++;
+      console.log(clickCount);
+
+      //add something
+      if (clickCount % 2 === 0) {
+        $(this).addClass("o");
+        $(this).html("<h1>O</h1>");
+        $("#whosturn").html("X's");
+      }
+      if (clickCount % 2 !== 0) {
+        $(this).addClass("x");
+        $(this).html("<h1>X</h1>");
+        $("#whosturn").html("O's");
+      }
     }
   });
 

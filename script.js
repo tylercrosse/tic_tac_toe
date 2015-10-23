6f3d2d494c8e11f2af4de8b7970a070914ac4083
Tyler <tylerscottcrosse@gmail.com>
2015-10-23 10:06:12 -0400

    completed get started requirements

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
+// on click add or remove class .active to td's
+
   $("td").on("click", function() {
-    $(this).addClass("active");
+    // if ($(this).hasClass("active")) {
+    //   $(this).removeClass("active");
+    // } else {
+      $(this).addClass("active");
+    // }
+  });
+
+//when you press reset remove class active from all td's
+  $(".reset").on("click", function(){
+    $("td").removeClass("active");
   });
 
 });

a85fa7b7b255a136929e783d8f33fc2b380a6acd
Tyler <tylerscottcrosse@gmail.com>
2015-10-23 10:26:18 -0400

    added click count and console logging xs and os

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+var clickCount = 0;
+
 $(document).ready(function() {
 
-// on click add or remove class .active to td's
+  // var turn = function(){
+  //   if (clickCount % 2 === 0) {
+  //     console.log("x");
+  //   }
+  // };
+  // on click add or remove class .active to td's
 
   $("td").on("click", function() {
     // if ($(this).hasClass("active")) {
     //   $(this).removeClass("active");
     // } else {
-      $(this).addClass("active");
-    // }
+    $(this).addClass("active");
+    clickCount++;
+    console.log(clickCount);
+    if (clickCount % 2 === 0) {
+      console.log("o");
+    }
+    if (clickCount % 2 !== 0) {
+      console.log("x");
+    }
   });
 
-//when you press reset remove class active from all td's
-  $(".reset").on("click", function(){
+  //when you press reset remove class active from all td's
+  $(".reset").on("click", function() {
     $("td").removeClass("active");
   });
 
